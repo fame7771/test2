bd3d734e73bbb5cff8f520177ab6d6658c2b4c39
woah <bioche>
2018-04-04 16:17:08 -0500

    Initial commit

---
+++ loader/Loader_FRAMEWORK/CLogin.h
@@ -0,0 +1,24 @@
+#pragma once
+
+struct login
+{
+	BOOL bSuccess;
+	BOOL bCSGO;
+};
+
+class CLogin
+{
+public:
+	void GetMACAddress(std::string &sOut);
+	BOOL isValidInput(char* pszInput);
+	std::string getCurrentTime();
+	VOID processLogin(char* pszUsername, char* pszPassword);
+	VOID initComboBox(HWND hCombo);
+	login getSession();
+	BOOL IsOutdated();		
+private:
+	login m_login;			
+	std::string aes_key = "Generate key here"; //https://aesencryption.net/
+	std::string aes_iv = "Generate key here"; //https://aesencryption.net/
+
+}; extern CLogin g_Login;
